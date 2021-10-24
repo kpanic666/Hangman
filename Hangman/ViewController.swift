8603064f46fcb9e0d4a83d32339b6f64b2db9c51
Andrei Korikov <kpanic666@gmail.com>
2021-10-24 16:36:48 +0500

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Andrei Korikov on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
