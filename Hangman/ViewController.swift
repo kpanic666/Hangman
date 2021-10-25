ff60ba1bc0624bc58926161572d55511e32eda75
Andrei Korikov <kpanic666@gmail.com>
2021-10-26 00:22:44 +0500

    Win, lose conditions is done. The game is finally in playable state.

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet var wordLabel: UILabel!
     @IBOutlet var hangmanImage: UIImageView!
     @IBOutlet var buttonsView: UIView!
+    @IBOutlet var numOfLettersLabel: UILabel!
     
+    var usedButtons = [String : UIButton]()
+    var currentWord = ""
+    var lines = [String]()
     var clues = [String]()
     var words = [String]()
     var fails = 0 {
@@ -30,29 +34,82 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadWordsAndClues()
-        setNewWord()
+        restartGame(action: nil)
     }
     
     func loadWordsAndClues() {
-        let data = String(localized: .init("words"))
-        var lines = data.components(separatedBy: "\n")
+        if lines.isEmpty {
+            let data = String(localized: "words")
+            lines = data.components(separatedBy: "\n")
+        }
+        
         lines.shuffle()
         
+        words.removeAll(keepingCapacity: true)
+        clues.removeAll(keepingCapacity: true)
         for line in lines {
             let parts = line.components(separatedBy: ": ")
             words.append(parts[0].uppercased())
             clues.append(parts[1])
         }
     }
-
+    
     func setNewWord() {
+        clueLabel.text = clues[round]
+        currentWord = words[round]
+        
+        let wordLen = currentWord.count
+        wordLabel.text = String(repeating: "?", count: wordLen)
+        numOfLettersLabel.text = "\(wordLen) \(String(localized: "letters"))"
+        
         fails = 0
         round += 1
         
-        let index = round - 1
-        clueLabel.text = clues[index]
-        wordLabel.text = words[index]
+        usedButtons.forEach { (_, btn) in
+            btn.isHidden = false
+        }
+        usedButtons.removeAll()
+    }
+    
+    @IBAction func letterTapped(_ sender: UIButton) {
+        guard let btnText = sender.titleLabel?.text else { return }
+        
+        usedButtons[btnText] = sender
+        sender.isHidden = true
+        
+        var newLabel = ""
+        currentWord.forEach { char in
+            newLabel.append(usedButtons[String(char)] != nil ? char : "?")
+        }
+        
+        if wordLabel.text != newLabel {
+            wordLabel.text = newLabel
+            
+            if newLabel == currentWord {
+                if round < words.count {
+                    setNewWord()
+                } else {
+                    showAlertMsg(title: String(localized: "winTitle"), message: String(localized: "winGameMsg"))
+                }
+            }
+        } else {
+            fails += 1
+            if fails >= 7 {
+                showAlertMsg(title: String(localized: "loseTitle"), message: String(localized: "newGameMsg"))
+            }
+        }
+    }
+    
+    func showAlertMsg(title: String?, message: String?) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: restartGame))
+        present(ac, animated: true)
+    }
+    
+    func restartGame(action: UIAlertAction?) {
+        round = 0
+        loadWordsAndClues()
+        setNewWord()
     }
 }
 
