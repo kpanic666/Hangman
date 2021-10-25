80f4fd2e99aac7e62c667c31630b05f4ce188eec
Andrei Korikov <kpanic666@gmail.com>
2021-10-25 13:05:46 +0500

    - Add main screens in 2 languages (en, ru) - Load words from strings file

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var roundLabel: UILabel!
+    @IBOutlet var clueLabel: UILabel!
+    @IBOutlet var wordLabel: UILabel!
+    @IBOutlet var hangmanImage: UIImageView!
+    @IBOutlet var buttonsView: UIView!
+    
+    var clues = [String]()
+    var words = [String]()
+    var fails = 0 {
+        didSet {
+            hangmanImage?.image = fails > 0 ? UIImage(named: "hang\(fails)") : nil
+        }
+    }
+    var round = 0 {
+        didSet {
+            roundLabel.text = "\(NSLocalizedString("round", comment: "")) \(round)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadWordsAndClues()
+        setNewWord()
+    }
+    
+    func loadWordsAndClues() {
+        let data = String(localized: .init("words"))
+        var lines = data.components(separatedBy: "\n")
+        lines.shuffle()
+        
+        for line in lines {
+            let parts = line.components(separatedBy: ": ")
+            words.append(parts[0].uppercased())
+            clues.append(parts[1])
+        }
     }
 
-
+    func setNewWord() {
+        fails = 0
+        round += 1
+        
+        let index = round - 1
+        clueLabel.text = clues[index]
+        wordLabel.text = words[index]
+    }
 }
 
